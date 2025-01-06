c34bd3113e120256e025b9b43e0d70a728861220
MrBiedny <MrBiedny@gmail.com>
2025-01-06 17:45:31 +0100

    first commit

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Profile() {
+  async function handleLogout() {}
+
+  return (
+    <SafeAreaView>
+      <Text>profile</Text>
+    </SafeAreaView>
+  );
+}
